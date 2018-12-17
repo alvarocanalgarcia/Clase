3fab4836cb5e22b2d0f609df0add6913ea187283
Jónathan Heras <joheras@gmail.com>
2018-12-17 09:44:21 +0100

    Update TestsSortedSet.java

--- src/test/java/TestsSortedSet.java
+++ src/test/java/TestsSortedSet.java
@@ -3,7 +3,10 @@ import static org.junit.Assert.*;
 import java.util.Comparator;
 
 import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestsSortedSet {
 	
 	// Unas clases auxiliares para los tests
